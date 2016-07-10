5d28a5996c9fcda3c19d243527860b9e9df52c74
JEAN-PIERRE MENA <pythonrubylang@gmail.com>
2016-07-10 11:15:44 +0200

    Build Proces Pass and formula added

--- app/src/main/java/eu/jpmena/loancalculator/MainActivity.java
+++ app/src/main/java/eu/jpmena/loancalculator/MainActivity.java
@@ -1,5 +1,6 @@
 package eu.jpmena.loancalculator;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,87 +9,100 @@ import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-        // currency and percent formatter objects
-        private static final NumberFormat currencyFormat =
-                NumberFormat.getCurrencyInstance();
-        private static final NumberFormat percentFormat =
-                NumberFormat.getPercentInstance();
+    // currency and percent formatter objects
+    private static final NumberFormat currencyFormat =
+            NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percentFormat =
+            NumberFormat.getPercentInstance();
 
-        private double loanAmount = 0.0; // the amount I have to (entered by the user)
-        private double rate = 0.15; // rate I negociated for  my loan percentage
-        private int years = 1; //initial number of years for my loan
-        private TextView amountTextView; // shows formatted bill amount
-        private TextView rateTextView; // shows rate Amount I should negociate
-        private TextView yearsTextView; // shows the number of years I expect to pay ...
-        private TextView monthlyTextView; // shows calculated value have to pay (to be compared with my actual rent)
+    private double loanAmount = 0.0; // the amount I have to (entered by the user)
+    private double rate = 0.15; // rate I negociated for  my loan percentage
+    private int years = 1; //initial number of years for my loan
+    private TextView amountTextView; // shows formatted bill amount
+    private TextView rateTextView; // shows rate Amount I should negociate
+    private TextView yearsTextView; // shows the number of years I expect to pay ...
+    private TextView monthlyTextView; // shows calculated value have to pay (to be compared with my actual rent)
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
-        // called when the activity is first created
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState); // call superclass onCreate
-            setContentView(R.layout.activity_main); // inflate the GUI
+    // called when the activity is first created
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); // call superclass onCreate
+        setContentView(R.layout.activity_main); // inflate the GUI
 
-            // get references to programmatically manipulated TextViews
-            amountTextView = (TextView) findViewById(R.id.amountTextView);
-            rateTextView = (TextView) findViewById(R.id.rateTextView);
-            yearsTextView = (TextView) findViewById(R.id.yearsTextView);
-            monthlyTextView = (TextView) findViewById(R.id.monthlyTextView);
-            monthlyTextView.setText(currencyFormat.format(0));
+        // get references to programmatically manipulated TextViews
+        amountTextView = (TextView) findViewById(R.id.amountTextView);
+        rateTextView = (TextView) findViewById(R.id.rateTextView);
+        yearsTextView = (TextView) findViewById(R.id.yearsTextView);
+        monthlyTextView = (TextView) findViewById(R.id.monthlyTextView);
+        monthlyTextView.setText(currencyFormat.format(0));
 
-            // set amountEditText's TextWatcher
-            EditText amountEditText =
-                    (EditText) findViewById(R.id.amountEditText);
-            amountEditText.addTextChangedListener(amountEditTextWatcher);
+        // set amountEditText's TextWatcher
+        EditText amountEditText =
+                (EditText) findViewById(R.id.amountEditText);
+        amountEditText.addTextChangedListener(amountEditTextWatcher);
 
-            // set percentSeekBar's OnSeekBarChangeListener
-            SeekBar rateSeekBar =
-                    (SeekBar) findViewById(R.id.rateSeekBar);
-            rateSeekBar.setOnSeekBarChangeListener(rateBarListener);
+        // set percentSeekBar's OnSeekBarChangeListener
+        SeekBar rateSeekBar =
+                (SeekBar) findViewById(R.id.rateSeekBar);
+        rateSeekBar.setOnSeekBarChangeListener(rateBarListener);
 
-            // set percentSeekBar's OnSeekBarChangeListener
-            SeekBar yearsSeekBar =
-                    (SeekBar) findViewById(R.id.yearsSeekBar);
-            rateSeekBar.setOnSeekBarChangeListener(yearsBarListener);
-        }
+        // set percentSeekBar's OnSeekBarChangeListener
+        SeekBar yearsSeekBar =
+                (SeekBar) findViewById(R.id.yearsSeekBar);
+        rateSeekBar.setOnSeekBarChangeListener(yearsBarListener);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
 
-        /* calculate how much money you owe for the loan
-        ** following the underneath formula !!!
-        ** http://forum.actufinance.fr/formule-mathematique-de-vpm-vc-et-va-P45065/
-         */
-        private void calculate() {
-            // format percent and display in percentTextView
-            percentTextView.setText(percentFormat.format(percent));
+    /* calculate how much money you owe for the loan
+    ** following the underneath formula !!!
+    ** http://forum.actufinance.fr/formule-mathematique-de-vpm-vc-et-va-P45065/
+     */
+    private void calculate() {
+        // format rate and display in rateTextView
+        rateTextView.setText(percentFormat.format(rate));
 
-            // calculate the tip and total
-            double tip = billAmount * percent;
-            double total = billAmount + tip;
+        // format rate and display in rateTextView
+        yearsTextView.setText(new Integer(years));
+        // calculate the tip and total
+        double monthlyamount = Math.pow(loanAmount * rate / (1 - (1 + rate)), -years * 12.0);
 
-            // display tip and total formatted as currency
-            tipTextView.setText(currencyFormat.format(tip));
-            totalTextView.setText(currencyFormat.format(total));
-        }
+        monthlyTextView.setText(currencyFormat.format(monthlyamount));
+    }
 
-        // listener object for the SeekBar's progress changed events
-        private final SeekBar.OnSeekBarChangeListener rateBarListener =
-                new SeekBar.OnSeekBarChangeListener() {
-                    // update percent, then call calculate
-                    @Override
-                    public void onProgressChanged(SeekBar seekBar, int progress,
-                                                  boolean fromUser) {
-                        rate = progress / 100.0; // set percent based on progress
-                        calculate(); // calculate and display tip and total
-                    }
+    // listener object for the SeekBar's progress changed events
+    private final SeekBar.OnSeekBarChangeListener rateBarListener =
+            new SeekBar.OnSeekBarChangeListener() {
+                // update percent, then call calculate
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress,
+                                              boolean fromUser) {
+                    rate = progress / 100.0; // set percent based on progress
+                    calculate(); // calculate and display tip and total
+                }
 
-                    @Override
-                    public void onStartTrackingTouch(SeekBar seekBar) { }
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                }
 
-                    @Override
-                    public void onStopTrackingTouch(SeekBar seekBar) { }
-                };
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                }
+            };
 
     // listener object for the SeekBar's progress changed events
     private final SeekBar.OnSeekBarChangeListener yearsBarListener =
@@ -97,42 +111,85 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onProgressChanged(SeekBar seekBar, int progress,
                                               boolean fromUser) {
-                    percent = progress / 100.0; // set percent based on progress
+                    years = progress; // set percent based on progress
                     calculate(); // calculate and display tip and total
                 }
 
                 @Override
-                public void onStartTrackingTouch(SeekBar seekBar) { }
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                }
 
                 @Override
-                public void onStopTrackingTouch(SeekBar seekBar) { }
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                }
+            };
+
+    // listener object for the EditText's text-changed events
+    private final TextWatcher amountEditTextWatcher = new TextWatcher() {
+        // called when the user modifies the bill amount
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count) {
+
+            try { // get bill amount and display currency formatted value
+                loanAmount = Double.parseDouble(s.toString());
+                amountTextView.setText(currencyFormat.format(loanAmount));
+            } catch (NumberFormatException e) { // if s is empty or non-numeric
+                amountTextView.setText("");
+                loanAmount = 0.0;
             }
 
-        // listener object for the EditText's text-changed events
-        private final TextWatcher amountEditTextWatcher = new TextWatcher() {
-            // called when the user modifies the bill amount
-            @Override
-            public void onTextChanged(CharSequence s, int start,
-                                      int before, int count) {
+            calculate(); // update the tip and total TextViews
+        }
 
-                try { // get bill amount and display currency formatted value
-                    billAmount = Double.parseDouble(s.toString()) / 100.0;
-                    amountTextView.setText(currencyFormat.format(billAmount));
-                }
-                catch (NumberFormatException e) { // if s is empty or non-numeric
-                    amountTextView.setText("");
-                    billAmount = 0.0;
-                }
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
 
-                calculate(); // update the tip and total TextViews
-            }
+        @Override
+        public void beforeTextChanged(
+                CharSequence s, int start, int count, int after) {
+        }
+    };
 
-            @Override
-            public void afterTextChanged(Editable s) { }
+    @Override
+    public void onStart() {
+        super.onStart();
 
-            @Override
-            public void beforeTextChanged(
-                    CharSequence s, int start, int count, int after) { }
-        };
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://eu.jpmena.loancalculator/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
     }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://eu.jpmena.loancalculator/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+}
 
